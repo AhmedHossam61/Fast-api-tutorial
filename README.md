89aa2d173148541d057e923864874bec6c4c29bd
AhmedHossam61 <ahmedhoss.2023@gmail.com>
2026-02-22 17:10:42 +0200

    Add initial FastAPI application with health check and item retrieval endpoints; create .gitignore and update README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Fast-api-tutorial
+
+## installation
+you can istall all fastApi dependancies with 
+```bash
+pip install fastapi["all]
+```
+
+or use with mininum installation with
+```bash
+pip install fastapi uvicorn[standard]
+```
---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+items = [
+    { "id": 1, "name": "Item 1", "description": "This is item 1" },
+    { "id": 2, "name": "Item 2", "description": "This is item 2" },
+    { "id": 3, "name": "Item 3", "description": "This is item 3" },
+]
+
+# this is the home endpoint
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+
+@app.get("/items")
+def get_items():
+    return items
+
+# to test it "http://127.0.0.1:8000/items/3"
+@app.get("/items/{item_id}")
+def get_id(item_id:int):
+    for item in items:
+        if item["id"] == item_id:
+            return item
+    return {"error": "Item not found"}
